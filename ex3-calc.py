79f521b376876231e5929f05baa2b1a8ce0f40a3
NotKevin-sandbox <60230861+NotKevin-sandbox@users.noreply.github.com>
2020-01-24 14:52:49 -0500

    Add files via upload

---
+++ ex3-calc.py
@@ -0,0 +1,6 @@
+# Find the temperature in degrees Fahrenheit
+# when it is 9.4 degrees Centigrade
+
+# Note the general formaula given degrees Fahrenheit (F) is
+# C = F * (9/5) + 32
+print("It is ",float(9.4 * (9/5) + 32)," degrees Fahrenheit")
---
+++ ex3.py
@@ -0,0 +1,50 @@
+# This line displays the string "I will now count my chickens:"
+print("I will now count my chickens:")
+
+# This line displays the string "Hens" and simplifies the expression
+# (25 + 30 / 6) and displays the answer.
+print("Hens",float( 25 + 30 / 6))
+
+# This line displays the string "Roosters" and simplifies the expression
+# (100 - 25 * 3 % 4) and displays the answer.
+print("Roosters", float(100 - 25 * 3 % 4))
+
+# This line displays the string "Now I will count the eggs:"
+print("Now I will count the eggs:")
+
+# This line simplifies the expression (3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+# and displays the answer.
+print(float(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6))
+
+# This line displays the string "Is it true that 3 + 2 < 5 - 7?"
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# This line evaluates the expression (3 + 2 < 5 - 7) and displays
+# the answer.
+print(3 + 2 < 5 - 7)
+
+# This line displays the string "What is 3 + 2?" and simplifies
+# the expression (3 + 2) and displays the answer.
+print("What is 3 + 2?", float(3 + 2))
+
+# This line displays the string "What is 5 - 7?" and simplifies
+# the expression (5 - 7) and displays the answer.
+print("What is 5 - 7?", float(5 - 7))
+
+# This line displays the string "Oh, that's why it's False."
+print("Oh, that's why it's False.")
+
+# This line displays the string "How about some more."
+print("How about some more.")
+
+# This line displays the string "Is it greater?" and evaluates
+# the expression (5 > -2) and displays the answer.
+print("Is it greater?", 5 > -2)
+
+# This line displays the string "Is it greater or equal?" and evaluates
+# the expression (5 >= -2) and displays the answer.
+print("Is it greater or equal?", 5 >= -2)
+
+# This line displays the string "Is it less or equal?" and evaluates
+# the expression (5 <= -2) and displays the answer.
+print("Is it less or equal?", 5 <= -2)

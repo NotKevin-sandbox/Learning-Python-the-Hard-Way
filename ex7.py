d57f3cbc7e692cf377b76e2311e9ea1d20ef5a16
Matthew Grabala <60230861+NotKevin-sandbox@users.noreply.github.com>
2020-01-25 10:26:04 -0500

    Update ex7.py

--- ex7.py
+++ ex7.py
@@ -11,46 +11,46 @@ print("And everywhere that Mary went.")
 # concatenates string "." with itself 10 times then displays result
 print("." * 10) # what'd that do?
 
-# assigns variable end1 to string "C"
+# assigns variable (end1) to string "C"
 end1 = "C"
 
-# assigns variable end2 to string "h"
+# assigns variable (end2) to string "h"
 end2 = "h"
 
-# assigns variable end3 to string "e"
+# assigns variable (end3) to string "e"
 end3 = "e"
 
-# assigns variable end4 to string "e"
+# assigns variable (end4) to string "e"
 end4 = "e"
 
-# assigns variable end5 to string "s"
+# assigns variable (end5) to string "s"
 end5 = "s"
 
-# assigns variable end6 to string "e"
+# assigns variable (end6) to string "e"
 end6 = "e"
 
-# assigns variable end7 to string "B"
+# assigns variable (end7) to string "B"
 end7 = "B"
 
-# assigns variable end8 to string "u"
+# assigns variable (end8) to string "u"
 end8 = "u"
 
-# assigns variable end9 to string "r"
+# assigns variable (end9) to string "r"
 end9 = "r"
 
-# assigns variable end10 to string "g"
+# assigns variable (end10) to string "g"
 end10 = "g"
 
-# assigns variable end11 to string "e"
+# assigns variable (end11) to string "e"
 end11 = "e"
 
-# assigns variable end12 to string "r"
+# assigns variable (end12) to string "r"
 end12 = "r"
 
 # watch that comma at the end.  try removing it to see what happens
 
-# concatenates variables end(1-6), displays result, then displays " "(space)
+# concatenates variables [end(1-6)], displays result, then displays " "(space)
 print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
 
-# concatenates variables end(7-12), displays result
+# concatenates variables [end(7-12)], displays result
 print(end7 + end8 + end9 + end10 + end11 + end12)

ca53ad0ba634bccaf5d90c2c7062347bf065762f
Matthew Grabala <60230861+NotKevin-sandbox@users.noreply.github.com>
2020-01-25 18:43:17 -0500

    Add files via upload

---
+++ ex13a.py
@@ -0,0 +1,5 @@
+from sys import argv
+script, one, two, = argv
+
+input("Can you eat {} and {}? ".format(one, two))
+print("Yeah I thought so.")
---
+++ ex13b.py
@@ -0,0 +1,4 @@
+from sys import argv
+script, sugar, spice, everything_nice, chem_x = argv
+print("The only four things you need to have a good time are "
+        f"{sugar}, {spice}, {everything_nice}, and {chem_x}")

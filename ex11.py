02e4db103c80e54b01756903fde42a5ebabce4b9
Matthew Grabala <60230861+NotKevin-sandbox@users.noreply.github.com>
2020-01-25 12:01:09 -0500

    Add files via upload

---
+++ ex11.py
@@ -0,0 +1,13 @@
+print("How old are you?", end= ' ')
+age = input()
+print("How tall are you?", end= ' ')
+height = input()
+print("How much do you weigh?", end= ' ')
+weight = input()
+
+print(f"So you're {age} old, {height} tall and {weight} heavy.")
+
+#Study Drills
+
+food = input("What is your favorite food? ")
+print(f"Cool, I'll make {food} next time you come by.")
